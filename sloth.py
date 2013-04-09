880baa786123dca9f717b7ff1581f7f42e7c83c0
Constantine Molchanov <moigagoo@live.com>
2013-04-10 03:05:26 +0400

    [~] Requirements updated: configs added.

--- sloth.py
+++ sloth.py
@@ -3,20 +3,21 @@ from datetime import datetime
 
 import cherrypy
 import requests
+import configs
 
 import  sloth_conf
 
 def log(func):
     """Logger decorator"""
-    
+
     def logged(*args, **kwargs):
         with open('sloth.log', 'a') as log:
             log.writelines(
                 '%s -- %s(%s, %s): %s\n' % (
                     datetime.now().ctime(),
-                    func.__name__, 
-                    args, 
-                    kwargs, 
+                    func.__name__,
+                    args,
+                    kwargs,
                     func(*args, **kwargs)
                 )
             )
@@ -27,12 +28,12 @@ def log(func):
 @log
 def execute(action):
     """Executes command line command.
-    
+
     :param action: action to be executed
-    
+
     :returns: 'OK' if successful, Exception otherwise
     """
-    
+
     action = action.format(
         work_dir = sloth_conf.work_dir,
         branch = sloth_conf.branch
@@ -48,30 +49,30 @@ def execute(action):
 @log
 def transmit(payload, node):
     """Transmit payload to a node.
-    
+
     :param payload: payload to be transmitted
     :param node: complete node URL (with protocol and port, **without** the ``/sloth`` suffix)
-    
+
     :returns: response code
     """
-    
+
     return requests.post('%s/sloth' % node, data={'payload': payload})
 
 
 @cherrypy.expose
 def listen(payload):
     """Listens to Bitbucket commit payloads.
-    
+
     :param payload: BitBucket commit payload
     """
-    
+
     #only POST requests are considered valid
     if not cherrypy.request.method == 'POST':
             raise cherrypy.HTTPError(405)
-    
+
     for action in sloth_conf.actions:
         execute(action)
-        
+
     for node in sloth_conf.nodes:
         transmit(payload, node)
 
