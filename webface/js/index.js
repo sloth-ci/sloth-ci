c5c2c5c6fe55ea2f1f5c68583df60cc85b032e63
Konstantin Molchanov <moigagoo@live.com>
2013-04-26 00:44:41 +0400

    [+] Webface: Logs are now listed in reverse; user approving added.

--- webface/js/index.js
+++ webface/js/index.js
@@ -3,15 +3,15 @@ $(function(){
         $('#tab_users').parent().removeClass('active');
         $(this).parent().addClass('active');
 
-        $('#table_logs').show();
-        $('#table_users').hide();
+        $('#logs').show();
+        $('#users').hide();
     });
 
     $('#tab_users').on('click', function(){
         $('#tab_logs').parent().removeClass('active');
         $(this).parent().addClass('active');
 
-        $('#table_users').show();
-        $('#table_logs').hide();
+        $('#users').show();
+        $('#logs').hide();
     });
 });
