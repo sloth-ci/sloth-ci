5c5b4745488cf6888f1187c43b4bdba0dea881cb
Konstantin Molchanov <moigagoo@live.com>
2013-05-03 19:12:46 +0400

    [!] Webface removed.

--- sloth.py
+++ sloth.py
@@ -11,14 +11,10 @@ import cherrypy
 import requests
 import configs
 
-from mako.template import Template
-from mako.lookup import TemplateLookup
-
 
 class Sloth:
     def __init__(self, config):
         self.config = config
-        self.lookup = TemplateLookup(directories=self.config['server']['webface_dir'])
 
         file_handler = logging.FileHandler(self.config['log'], 'a+')
         formatter = logging.Formatter(
@@ -123,131 +119,6 @@ class Sloth:
             for node in self.config['nodes']:
                 self.broadcast(payload, node)
 
-    def user_validate_credentials(self, login, password):
-        """Validates webface login credentials.
-
-        :param login: Login
-        :param password: Password
-
-        :returns: True if the credentials are valid, False otherwise
-        """
-
-        if not login or not password:
-            return False
-
-        with sqlite3.connect(self.config['db']) as db_connection:
-            db_cursor = db_connection.cursor()
-
-            db_cursor.execute('CREATE TABLE IF NOT EXISTS Users(Id INTEGER PRIMARY KEY, Login TEXT, Hash TEXT, Status CHAR)')
-
-            try:
-                hash, status = db_cursor.execute('SELECT Hash, Status from Users WHERE Login = ?', (login,)).fetchone()
-            except:
-                return False
-
-        if status == 'A' and md5(password.encode()).hexdigest() == hash:
-            return True
-
-        return False
-
-    def user_register(self, r_login, r_password):
-        """Registers new webface user.
-
-        :param r_login: Login
-        :param r_password: Password
-        """
-
-        with sqlite3.connect(self.config['db']) as db_connection:
-            db_cursor = db_connection.cursor()
-
-            r_hash = md5(r_password.encode()).hexdigest()
-
-            r_status = db_cursor.execute('SELECT * FROM Users').fetchone() and 'P' or 'A'
-
-            db_cursor.execute('INSERT INTO Users(Login, Hash, Status) VALUES (?, ?, ?)', (r_login, r_hash, r_status))
-
-            db_connection.commit()
-
-    def user_update(self, user_statuses):
-        """Updates user statuses.
-
-        :param user_statuses: Dictionary of user ID and status to set
-        """
-
-        with sqlite3.connect(self.config['db']) as db_connection:
-            db_cursor = db_connection.cursor()
-
-            for uid, status in user_statuses.items():
-                db_cursor.execute('UPDATE Users SET Status = ? WHERE Id = ?', (status, uid))
-
-            db_connection.commit()
-
-    def get_log_lines(self):
-        """Read lines from the log file in reverse and format them to a dictionaries.
-
-        :yields: {'timestamp': timestamp, 'name': logger name, 'level': message level, 'message': message text}
-        """
-
-        for line in reversed(open(self.config['log']).readlines()):
-            yield dict(
-                zip(
-                    [
-                        'timestamp',
-                        'name',
-                        'level',
-                        'message'
-                    ],
-                    list(map(lambda __: __.strip(), line.split('|')))
-                )
-            )
-
-    def get_users(self):
-        """Gets users and formats them to dictionaries
-
-        :yields: {'id': uid, 'login': login, 'status', status ('A' or 'P')}
-        """
-
-        with sqlite3.connect(self.config['db']) as db_connection:
-            db_cursor = db_connection.cursor()
-
-            db_cursor.execute('CREATE TABLE IF NOT EXISTS Users(Id INTEGER PRIMARY KEY, Login TEXT, Hash TEXT, Status CHAR)')
-
-            for uid, login, status in db_cursor.execute('SELECT Id, Login, Status from Users'):
-                yield {'id': uid, 'login': login, 'status': status}
-
-    @cherrypy.expose
-    def webface(self, login=None, password=None, r_login=None, r_password=None, r_password2=None, **user_statuses):
-        """Renders webface.
-
-        Handles signins and signups, user status updating.
-
-        """
-
-        if cherrypy.request.method == 'GET':
-            tmpl = self.lookup.get_template('login.html')
-
-            return tmpl.render()
-
-        elif cherrypy.request.method == 'POST':
-            if self.user_validate_credentials(login, password) or self.user_validate_credentials(r_login, r_password):
-                tmpl = self.lookup.get_template('index.html')
-
-                lines = self.get_log_lines()
-
-                users = self.get_users()
-
-                return tmpl.render(login=login, lines=lines, users=users)
-
-            elif r_login and r_password == r_password2:
-                self.user_register(r_login, r_password)
-
-            elif user_statuses:
-                self.user_update(user_statuses)
-
-            raise cherrypy.HTTPRedirect(self.config['server']['path'] + '/webface/')
-        else:
-            raise cherrypy.HTTPError(405)
-
     def run(self):
         """Runs CherryPy loop to listen for payload."""
 
@@ -257,18 +128,6 @@ class Sloth:
         })
 
         cherrypy.tree.mount(self.listener, self.config['server']['path'])
-
-        cherrypy.tree.mount(
-            self.webface,
-            self.config['server']['path'] + '/webface',
-            {
-                '/': {
-                    'tools.staticdir.root': os.path.abspath(self.config['server']['webface_dir']),
-                    'tools.staticdir.on': True,
-                    'tools.staticdir.dir': ''
-                }
-            }
-        )
 
         cherrypy.engine.autoreload.files.add(self.config.config_file)
 
