02eae81fb70cf8ada2b906e3a9effde409d6c9e8
Constantine Molchanov <moigagoo@live.com>
2013-06-30 08:38:33 +0400

    [~] Version updated.

--- sloth/__init__.py
+++ sloth/__init__.py
@@ -9,7 +9,7 @@ The repo is at `bitbucket.org/moigagoo/configs <https://bitbucket.org/moigagoo/c
 """
 
 __title__ = 'sloth-ci'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Konstantin Molchanov'
 __license__ = 'MIT'
 
