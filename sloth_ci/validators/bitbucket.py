9c165c17cd391bc76925f05fba2ebd7a82a86e61
Konstantin Molchanov <kmolchanov@cs-cart.com>
2013-11-05 19:02:41 +0400

    0.2.9: BitBucket validator was erroneous. Fixed.

--- sloth_ci/validators/bitbucket.py
+++ sloth_ci/validators/bitbucket.py
@@ -20,10 +20,10 @@ def validate(request, validation_data):
         repo = parsed_payload['repository']['owner'] + '/' + parsed_payload['repository']['slug']
         branch = parsed_payload['commits'][-1]['branch']
 
-        if repo != data['repo']:
+        if repo != validation_data['repo']:
             return (False, 'Payload validation failed: repo mismatch. Repo: {repo}', {'repo': repo})
-        
+
         return (True, 'Payload validated. Branch: {branch}', {'branch': branch})
-    
+
     except Exception as e:
         return (False, 'Payload validation failed: %s' % e, {})
