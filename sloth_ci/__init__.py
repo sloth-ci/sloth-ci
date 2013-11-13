2e37bb346803f6faf159df12da8551e59fbb4ab2
Constantine Molchanov <moigagoo@live.com>
2013-11-14 00:34:54 +0400

    Docstrings: Typos fixed.

--- sloth_ci/__init__.py
+++ sloth_ci/__init__.py
@@ -1,9 +1,9 @@
 """
-*******
-Sloth
-*******
+********
+Sloth CI
+********
 
-**Sloth** is a lightweight CI script to perform push-based actions.
+**Sloth CI** is a lightweight CI script.
 
 The repo is at `bitbucket.org/moigagoo/configs <https://bitbucket.org/moigagoo/configs>`_.
 """
