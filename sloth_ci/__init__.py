6fa877503abba882b0efe561be43d450afda93c6
Constantine Molchanov <moigagoo@live.com>
2014-03-02 23:05:10 +0400

    0.4.3: IP validation added.

--- sloth_ci/__init__.py
+++ sloth_ci/__init__.py
@@ -9,7 +9,7 @@ The repo is at `bitbucket.org/moigagoo/sloth-ci <https://bitbucket.org/moigagoo/
 """
 
 __title__ = 'sloth-ci'
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'Konstantin Molchanov'
 __license__ = 'MIT'
 
