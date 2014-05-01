8a028aa9b519bc5d5b655456db37ae8337144db5
Constantine Molchanov <moigagoo@live.com>
2014-05-01 17:11:26 +0400

    .hgignore updated; Docs: Wrong path was added to sys.path. Fixed.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../sloth_ci'))
+sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
