b4f9f8946a548a985536a3ca1ecfc71fb784cbc5
Constantine Molchanov <moigagoo@live.com>
2014-06-04 01:13:06 +0400

    Ext: Docker: Reference to config section added (and working, biatch!).

--- sloth_ci/ext/docker.py
+++ sloth_ci/ext/docker.py
@@ -6,7 +6,7 @@ def extend(cls):
         def __init__(self, config):
             super().__init__(config)
 
-            self.foo = 'qwe'
+            self.foo = config['docker']['foo']
 
         def execute(self, action):
             print('Execute %s' % action)
