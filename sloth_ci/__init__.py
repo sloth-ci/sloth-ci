50aece4f0cc54fa0fdc6ba158b8f10f3a787ffe7
Constantine Molchanov <moigagoo@live.com>
2014-06-22 23:05:56 +0400

    0.7.1: Validators and extensions instructions added.

--- sloth_ci/__init__.py
+++ sloth_ci/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'sloth-ci'
 __description__ = 'Simple app for automatization of everything'
 __long_description__ = 'Sloth CI: CI for Humans. A simple yet extendable automatization app.'
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
