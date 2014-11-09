6b0629540ce308d1128ad28a52ed884dd89dc887
Konstantin Molchanov <moigagoo@live.com>
2014-11-09 20:23:36 +0300

    New util added: get_configs.

--- sloth_ci/utils.py
+++ sloth_ci/utils.py
@@ -3,6 +3,26 @@ from os.path import isdir, isfile, join, abspath, expanduser
 from os import listdir, stat
 from cherrypy.process.plugins import Monitor
 
+from yaml import load
+
+
+def get_configs(location):
+    '''Recursively get all config files from a ceratin location (can be a directory or a single file).
+    
+    :param location: path to a directory or config file
+    '''
+
+    if isfile(location):
+        try:
+            yield load(open(location))
+
+        except:
+            pass
+
+    elif isdir(location):
+        for item in (join(location, _) for _ in listdir(location)):
+            yield from get_configs(item)
+
 
 def get_config_files(config_locations):
     '''Generate a set of config files and config directories for Sloth apps.
