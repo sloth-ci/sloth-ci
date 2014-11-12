917fcc37bea40fbc082d5019f8548b5fa455bc89
Konstantin Molchanov <moigagoo@live.com>
2014-11-13 01:42:15 +0300

    API: Error page is now a plaintext message; some additional error checking added.

--- sloth_ci/api.py
+++ sloth_ci/api.py
@@ -1,6 +1,6 @@
 from cherrypy import expose
 from cherrypy import tools
-from cherrypy import HTTPError
+from cherrypy import HTTPError, request
 
 from cherrypy.lib.auth_basic import checkpassword_dict
 
@@ -9,10 +9,13 @@ class API:
     def __init__(self, bed):
         self.bed = bed
         
-        auth = self.bed.sconfig['api_auth']
+        auth = self.bed.config['api_auth']
 
         self.listener = self._make_listener({auth['login']: auth['password']})
     
+    def _handle_error(self, status, message, traceback, version):
+        return message
+
     def _make_listener(self, auth_dict, realm='sloth-ci'):
         '''Get a basic-auth-protected listener function for the API endpoint.
         
@@ -33,6 +36,8 @@ class API:
             :param params: a single object, a list, or a dict of params for the action.
             '''
 
+            request.error_page = {'default': self._handle_error}
+
             if action == 'create-app':
                 if not 'config_source' in kwargs:
                     raise HTTPError(400, 'Missing parameter config_source')
@@ -41,7 +46,10 @@ class API:
                     return self.bed.add_sloth(kwargs['config_source'])
 
                 except KeyError as e:
-                    raise HTTPError(409, 'Listen point %s is taken' % e)
+                    raise HTTPError(500, 'The %s param is missing in the config' % e)
+
+                except ValueError as e:
+                    raise HTTPError(409, 'Listen point %s is already taken' % e)
 
                 except Exception as e:
                     raise HTTPError(500, 'Could not create app: %s' % e)
