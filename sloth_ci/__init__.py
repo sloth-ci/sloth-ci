4a55cbea6dfcaaade551e404691228b81a463031
Constantine Molchanov <moigagoo@live.com>
2014-11-27 00:17:50 +0300

    Version updated to 1.0.6.

--- sloth_ci/__init__.py
+++ sloth_ci/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'sloth-ci'
 __description__ = 'Simple app for automatization of everything'
 __long_description__ = 'Sloth CI: CI for Humans. A simple and extendable automatization app.'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
