791a7c23018f11e23e17f7aa38f9a1efd0889f2f
Constantine Molchanov <moigagoo@live.com>
2014-12-06 22:01:26 +0300

    Ext: The db_app_logs extension added.

---
+++ sloth_ci/ext/db_app_logs.py
@@ -0,0 +1,29 @@
+def extend(cls, extension):
+    from os.path import abspath, join, exists
+    from os import makedirs
+
+    import logging
+
+    from ..util import SqliteHandler
+
+
+    class Sloth(cls):
+        def __init__(self, config):
+            super().__init__(config)
+            
+            db_app_log_config = extension['config']
+            
+            db_app_log_handler = SqliteHandler(db_app_log_config['db'], db_app_log_config['table'])
+
+            db_app_log_handler.setLevel(logging.DEBUG)
+            
+            self.logger.addHandler(db_app_log_handler)
+
+            self.log_handlers[extension['name']] = db_app_log_handler
+
+        def stop(self):
+            super().stop()
+            self.logger.removeHandler(self.log_handlers.pop(extension))
+
+
+    return Sloth
