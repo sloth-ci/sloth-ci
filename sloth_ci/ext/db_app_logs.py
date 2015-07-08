b83e0e7d06ab5eea9aa25c89c5cd65dbdb8d21d0
Constantine Molchanov <moigagoo@live.com>
2015-07-08 20:05:11 +0300

    Extensions: DB App Logs: Unused imports removed.

--- sloth_ci/ext/db_app_logs.py
+++ sloth_ci/ext/db_app_logs.py
@@ -1,7 +1,4 @@
 def extend(cls, extension):
-    from os.path import abspath, join, exists
-    from os import makedirs
-
     import logging
 
     from ..util import SqliteHandler
@@ -10,13 +7,13 @@ def extend(cls, extension):
     class Sloth(cls):
         def __init__(self, config):
             super().__init__(config)
-            
+
             db_app_log_config = extension['config']
-            
+
             db_app_log_handler = SqliteHandler(db_app_log_config['db'], db_app_log_config['table'])
 
             db_app_log_handler.setLevel(logging.DEBUG)
-            
+
             self.logger.addHandler(db_app_log_handler)
 
             self.log_handlers[extension['name']] = db_app_log_handler
