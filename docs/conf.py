03b7b8d72f32a55e66f8a97d533d56e5366714f0
Constantine Molchanov <moigagoo@live.com>
2015-12-02 10:48:26 +0300

    Docs: Theme changed to rtd.

--- docs/conf.py
+++ docs/conf.py
@@ -95,22 +95,22 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'description': 'Pure-Python Continuous Integration Tool',
-    'extra_nav_links': {
-        'Bitbucket': 'https://bitbucket.org/moigagoo/sloth-ci',
-        'PyPI': 'https://pypi.python.org/pypi/sloth-ci'
-        },
-    'github_button': False,
-    #'gratipay_user': 'moigagoo',
-    'show_powered_by': True,
-    'show_related': True
-}
+# html_theme_options = {
+#     'description': 'Pure-Python Continuous Integration Tool',
+#     'extra_nav_links': {
+#         'Bitbucket': 'https://bitbucket.org/moigagoo/sloth-ci',
+#         'PyPI': 'https://pypi.python.org/pypi/sloth-ci'
+#         },
+#     'github_button': False,
+#     #'gratipay_user': 'moigagoo',
+#     'show_powered_by': True,
+#     'show_related': True
+# }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = [alabaster.get_path()]
