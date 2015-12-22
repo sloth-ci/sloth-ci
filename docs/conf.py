16d41596bed69668366e54c8cf7f9af3c221b200
Konstantin Molchanov <moigagoo@live.com>
2015-12-23 01:28:21 +0300

    Docs: Makefile added, minor fixes applied.

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = '../sloth-weed-small.jpg'
+# html_logo = ''
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -207,7 +207,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '../sloth-weed-small.jpg'
+# latex_logo = ''
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
