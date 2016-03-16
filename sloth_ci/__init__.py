6ae582a57502388ad0c931f7dbfda053b3ded70b
Konstantin Molchanov <moigagoo@live.com>
2016-03-16 11:49:22 +0300

    Version bumped to 2.1.1.

--- sloth_ci/__init__.py
+++ sloth_ci/__init__.py
@@ -5,7 +5,7 @@ Just type the commands you'd run manually and let Sloth CI run them for you!
 
 __title__ = 'sloth-ci'
 __description__ = 'Lightweight CI tool'
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 __author__ = 'Konstantin Molchanov'
 __author_email__ = 'moigagoo@live.com'
 __license__ = 'MIT'
